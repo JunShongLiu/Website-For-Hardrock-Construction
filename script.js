ba36525a7adccf8aef37c52ffdad7a9356d67aa7
Jun Shong Liu <junshongliu@gmail.com>
2016-06-12 02:04:43 -0700

    Made footer and working on contact page

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.getElementById("navigation").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='contact.html'>Contact Us</a></li>" +
+"<li><a href='home.html'>Home</a></li>" +
+"<li><a href='work.html'>Recent Work</a></li>" +
+"<li><a href='price.html'>Prices and Sample Invoices</a></li>" +
+"<li><a href='contact.html'>Contact</a></li>" +
 "</ul>";
+
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Justin Liu. All rights reserved.</p>";
